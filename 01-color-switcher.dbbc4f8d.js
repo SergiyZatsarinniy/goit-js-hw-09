95a2edf29c642a192426ca0ffe84eeee88dfb940
SergiyZatsarinniy <122026108+SergiyZatsarinniy@users.noreply.github.com>
2023-05-29 22:16:22 +0000

    Deploying to gh-pages from @ SergiyZatsarinniy/goit-js-hw-09@2e6d74060b47fbb3492b44f3b7b75996a4ddbbfe 🚀

---
+++ 01-color-switcher.dbbc4f8d.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){})),t.stopBtn.addEventListener("click",stopChangeColor)}();
+//# sourceMappingURL=01-color-switcher.dbbc4f8d.js.map
